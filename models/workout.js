cfc5bdee90b69374cd98e5f515cd58539b8b1fe6
baloyd <danielbaloy@gmail.com>
2021-07-15 12:21:54 -0400

    started api routes; renamed db/model to fit assets

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema({
+const WorkoutSchema = new Schema({
 
   day: {
     type: Date,
@@ -51,6 +51,6 @@ const ExerciseSchema = new Schema({
 ]
 });
 
-const Exercise = mongoose.model("Exercise", ExerciseSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = Exercise;
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const logger = require("morgan");
+const mongojs = require("mongojs")
 const mongoose = require("mongoose");
 const path = require("path")
+const Workout = require("./models/workout.js");
 
 const PORT = process.env.PORT || 3000;
 
-const Exercise = require("./models/exerciseModel.js");
+
+const { db } = require("./models/workout.js");
+
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true }, { useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true,useCreateIndex: true,
+useFindAndModify: false,useUnifiedTopology: true  });
 
 
 //home routes
@@ -26,6 +30,52 @@ app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '/public/ex
 app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '/public/stats.html')));
 
 
+//api routes
+
+//get workouts
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({}, (err, data) =>{
+    dbWorkout.forEach(workout =>{
+      var total=0;
+      workout.exercises.forEach(e =>{
+        total += e.duration;
+      });
+      workout.totalDuration = total;
+    });
+    if(err) {
+      res.sendStatus(500);
+    }else {
+      res.json(data)
+    }
+  })
+})
+
+//create workout
+app.post("/api/workouts", (req, res) =>{
+  db.Workout.create(req.body, (err,data) =>{
+    if(err) {
+      res.sendStatus(500);
+    }else{
+      res.json(data);
+    }
+  })
+})
+
+//add to existing workout
+app.put("/api/workouts/:id", (req, res) =>{
+  db.Workout.findOneAndUpdate(
+    {_id: req.params.id},
+    {
+      $inc: {totalDuration: req.body.duration},
+      $push: {exercises:req.body}
+    },
+    {new:true}
+  ).then(dbWorkout =>{
+    res.json(dbWorkout);
+  }).catch(err =>{
+    res.json(err)
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
